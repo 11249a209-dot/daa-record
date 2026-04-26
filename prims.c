fec0ed3bf2d06650752ab20722087904ce590bb1
11249a209-dot <11249a209@kanchiuniv.ac.in>
2026-04-26 20:56:29 +0530

    Update prims.c

--- prims.c
+++ prims.c
@@ -10,3 +10,89 @@ Prim’s Algorithm
 6. Add that edge and the new vertex to the tree  
 7. Repeat steps 3–6  
 8. Continue until all vertices are included
+#include <stdio.h> 
+#include <stdlib.h> 
+#define V 5 // Number of vertices 
+ 
+// Function to find the vertex with minimum key value (Minimum edge weight) 
+int minKey(int key[], int visited[]) 
+{ 
+    int min = 999999; // Initialize min to a large value 
+    int min_index = 0; // Initialize min_index to 0 as it starts with vertex 0 
+ 
+    for (int i = 0; i < V; i++) 
+    { 
+        if (!visited[i] && key[i] < min) 
+        {  
+            min = key[i]; 
+            min_index = i; 
+        } 
+    } 
+    return min_index; 
+} 
+ 
+// Function to print MST 
+void printPrimMST(int parent[], int graph[V][V]) 
+{ 
+    printf("\nPrim's MST:\n"); 
+    printf("Edge \tWeight\n"); 
+ 
+    int total = 0; 
+    for (int i = 1; i < V; i++) 
+    { 
+        printf("%d - %d \t%d\n", parent[i], i, graph[i][parent[i]]); 
+        total += graph[i][parent[i]]; 
+    } 
+    printf("Total Cost = %d\n", total); 
+} 
+ 
+// Prim's Algorithm Implementation 
+void primMST(int graph[V][V]) 
+{ 
+    int parent[V];  // Stores MST 
+    int key[V];     // Minimum edge weight 
+    int visited[V]; // Track visited vertices 
+ 
+    // Step 1: Initialize 
+    for (int i = 0; i < V; i++) 
+    { 
+        key[i] = 999999; 
+        visited[i] = 0; 
+    } 
+ 
+    // Step 2: Start from vertex 0 
+    key[0] = 0; 
+    parent[0] = -1; 
+ 
+    
+    for (int count = 0; count < V - 1; count++) 
+    { 
+       
+        int u = minKey(key, visited); 
+        visited[u] = 1; 
+       
+        for (int v = 0; v < V; v++) 
+        { 
+            
+than key[v] 
+            if (graph[u][v] && !visited[v] && graph[u][v] < key[v]) 
+            { 
+                parent[v] = u; 
+                key[v] = graph[u][v]; 
+            } 
+        } 
+    } 
+    printPrimMST(parent, graph); 
+} 
+ 
+int main() 
+{
+    int graph[V][V] = { 
+        {0, 2, 3, 0, 0}, 
+        {2, 0, 5, 3, 0}, 
+        {3, 5, 0, 0, 4}, 
+        {0, 3, 0, 0, 2}, 
+        {0, 0, 4, 2, 0}},
+    primMST(graph); 
+    return 0; 
+}

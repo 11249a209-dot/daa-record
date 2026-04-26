42064bd295500af12d12d0539dd347459b0ba9ea
11249a209-dot <11249a209@kanchiuniv.ac.in>
2026-04-26 20:20:05 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -7,3 +7,34 @@ ALGORITHM:
 3.if n=1,return 1(base condition)
 4.recursively compute: f(n)=f(n-1)+f(n-2)
 5.display the returned value as the nth fibonacci term.
+  program:
+  #include <stdio.h> 
+  int main() 
+{
+  int n; 
+  int a = 0, b = 1, c; 
+  printf("Enter the number of terms (n): "); 
+  if (scanf("%d", &n) != 1 || n < 0) 
+{ 
+  printf("Invalid input. Please enter a positive integer.\n"); 
+  return 0; 
+} 
+  printf("Fibonacci Series: ");  
+  if (n >= 1) 
+  printf("%d ", a); 
+  if (n >= 2) 
+  printf("%d ", b); 
+  for (int i = 3; i <= n; i++) 
+  { 
+  c = a + b; 
+  printf("%d ", c); 
+  a = b; 
+  b = c; 
+}  
+  Otherwise, the nth term is 'b'. 
+  if (n == 1)
+  printf("\nFibonacci term %d = %d\n", n, a); 
+else 
+  printf("\nFibonacci term %d = %d\n", n, b);   
+     return 0; 
+}
